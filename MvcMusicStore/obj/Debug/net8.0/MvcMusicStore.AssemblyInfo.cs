649691ca6274c24e79fc4dbaded67f96fd7339b9
NoReply Amazon <no-reply@amazon.com>
2025-02-05 09:34:48 +0000

    [Commit] Iteration 12 #errors = 14 <= 16.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/MvcMusicStore/Controllers/StoreController.cs][CS0246][47,10] The type or namespace name 'ChildActionOnlyAttribute' could not be found (are you missing a using directive or an assembly reference?)``.

--- MvcMusicStore/obj/Debug/net8.0/MvcMusicStore.AssemblyInfo.cs
+++ MvcMusicStore/obj/Debug/net8.0/MvcMusicStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMusicStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb9fcb36b85a14d3738c2c2123f4f41719a018dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c3436872e985da21dbea886ac2f95e5e672653e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMusicStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMusicStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
